34747ae50c4daa5f58ebf39b63499b1b75623d50
Nicolas Chamorro <nicolaschamorro@cisde.co>
2020-11-26 11:38:58 -0500

    primer commit - portafolio cisde agregado

---
+++ resources/views/index.blade.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- meta -->
+  <title>Portafolio CISDE | Paginas web | Catalogo web | webcali</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="portafolio cisde, paginas web, catalogo web, webcali, webpagina, paginas, paginas web, diseño de paginas">
+  <meta name="description" content="CISDE portafolio, creacion de paginas web, catalogos web, paginas web, diseño de paginas de negocio, webcali digital, tarjetas de presentacion y logos.">
+  <meta name="subject" content="Productos digitales">
+  <meta name="author" content="Nicolas Chamorro">
+  <meta name="copyright" content="© CISDE">
+  <meta name="language" content="es">
+  <meta name="revisit-after" content="1 month">
+  <meta name="robots" content="index, follow">
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" >
+  <link rel="canonical" href="https://cisde.co/">
+
+  <!-- Open Graph  -->
+  <meta property="og:title" content="Portafolio CISDE | Paginas web | Catalogo web | webcali"/>
+  <meta property="og:type" content="article"/>
+  <meta property="og:url" content="https://cisde.co/"/>
+  <meta property="og:site_name" content="CISDE"/>
+  <meta property="og:image" content="https://smith-hoes.com/images/cisde/fondo.webp"/>
+  <meta property="og:image:alt" content="CISDE DIGITAL"/>	
+  <meta property="og:description" content="CISDE portafolio, creacion de paginas web, catalogos web, paginas web, diseño de paginas de negocio, webcali digital, tarjetas de presentacion y logos."/>
+  
+  <!-- TWITER -->
+  <meta name="twitter:card" content="summary" />	
+  <meta name="twitter:title" content="Portafolio CISDE | Paginas web | Catalogo web | webcali" >
+  <meta name="twitter:description" content="CISDE portafolio, creacion de paginas web, catalogos web, paginas web, diseño de paginas de negocio, webcali digital, tarjetas de presentacion y logos." >
+  <meta name="twitter:image" content="https://smith-hoes.com/images/cisde/fondo.webp" >
+  <meta name="twitter:site" content="@Nicolas63783814" >
+  <meta name="twitter:creator" content="@Nicolas63783814" >
+
+  <!-- REDES -->
+  <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "Organization",
+      "name": "CISDE",
+      "url": "https://julianalbertoa3.sg-host.com/",
+      "address": "Calle 74C # AC - 10",
+      "sameAs": [
+      "https://www.facebook.com/Cisde.web",
+      "https://www.instagram.com/cisdewdp",
+      "https://linkedin.com/in/cisdeweb"
+      ]
+    }
+  </script>
+
+  <!-- DATOS STRUCTURES SIN REALIZAR-->
+  <script type="application/ld+json">
+      {
+      "@context": "https://schema.org/",
+      "@type": "Product",
+      "name": "Catalogo online",
+      "image": [
+        "https://smith-hoes.com/images/cisde/fondo.webp"
+        ],
+      "description": "Portafolio web, ideal para mostrar tus productos o servicios a tus clientes",
+      "sku": "1",
+      "brand": {
+        "@type": "Brand",
+        "name": "Producto Digital"
+      },
+      "review": {
+        "@type": "Review",
+        "reviewRating": {
+          "@type": "Rating",
+          "ratingValue": "4.7",
+          "bestRating": "5"
+        },
+        "author": {
+          "@type": "Person",
+          "name": "CISDE"
+        }
+      },
+      "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "4.4",
+        "reviewCount": "2780"
+      },
+      "offers": {
+        "@type": "AggregateOffer",
+        "lowPrice": "150.000",
+        "highPrice": "200.000",
+        "priceCurrency": "COL"
+      }
+    }
+    </script>
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="{{asset('css/cisde/bootstrap/bootstrap.min.css')}}" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="{{asset('css/cisde/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('css/cisde/magnific-popup.css')}}" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="{{asset('css/cisde/style.css')}}" rel="stylesheet">
+
+  <!-- Responsive css -->
+  <link href="{{asset('css/cisde/responsive.css')}}" rel="stylesheet">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="{{asset('images/cisde/logo cisde webp.webp')}}">
+
+</head>
+
+<body>
+
+
+  <!-- start section header -->
+  <div id="header" class="home">
+
+    <div class="container">
+      <div class="header-content">
+        <h1>CISDE <span class="typed"></span></h1>
+        <h2 class="p-title ml-1">Centro de soluciones y desarrollo empresarial.</h2>
+        <ul class="list-unstyled list-social mt-3 ml-2">
+          <li><a href="https://www.facebook.com/Cisde.web"><i class="ion-social-facebook"></i></a></li>
+          <li><a href="https://www.instagram.com/cisdewdp"><i class="ion-social-instagram"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/cisdeweb/"><i class="ion-social-linkedin"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!-- End section header -->
+
+
+
+  <!-- start section portfolio -->
+  <div id="portfolio" class="text-center paddsection">
+
+    <div class="container">
+      <div class="section-title text-center">
+        <h2>Mi Portfolio</h2>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+
+          <div class="portfolio-list">
+
+            <ul class="nav list-unstyled" id="portfolio-flters">
+              <li class="filter filter-active" data-filter=".all">Todo</li>
+              <li class="filter" data-filter=".paginas">paginas web</li>
+              <li class="filter" data-filter=".tarjetas">Tarjetas de presentación</li>
+              <li class="filter" data-filter=".marketing">Marketing digital</li>
+              <li class="filter" data-filter=".logos">Logos</li>
+            </ul>
+
+          </div>
+
+          <div class="portfolio-container">
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all branding uikits paginas">
+              <a class="popup-img" href="{{asset('images/cisde/smith-shoes.webp')}}">
+                <img src="{{asset('images/cisde/smith-shoes.webp')}}" alt="smith shoes" height="200px">
+              </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all mockups uikits tarjetas">
+              <a class="popup-img" href="{{asset('images/cisde/tarjeta-julian.webp')}}">
+                <img src="{{asset('images/cisde/tarjeta-julian.webp')}}"  alt="julian alonso" height="200px">
+              </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all branding webdesig marketing">
+              <a class="popup-img" href="{{asset('images/cisde/facebook.webp')}}">
+                <img src="{{asset('images/cisde/facebook.webp')}}" alt="facebook" height="200px">
+              </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all mockups webdesign logos">
+              <a class="popup-img" href="{{asset('images/cisde/logo1.webp')}}">
+                <img src="{{asset('images/cisde/logo1.webp')}}" alt="logo 1" height="200px">
+              </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all branding uikits paginas">
+              <a class="popup-img" href="{{asset('images/cisde/maquillaje.biz.webp')}}">
+                <img src="{{asset('images/cisde/maquillaje.biz.webp')}}" alt="atomy maquillaje" height="200px">
+              </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all mockups uikits tarjetas">
+              <a class="popup-img" href="{{asset('images/cisde/nicolas-tarjeta.webp')}}">
+                <img src="{{asset('images/cisde/nicolas-tarjeta.webp')}}" alt="nicolas chamorro" height="200px">
+              </a>
+            </div>
+
+            
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all branding uikits marketing">
+              <a class="popup-img" href="{{asset('images/cisde/google.webp')}}">
+                <img src="{{asset('images/cisde/google.webp')}}" alt="google" height="200px">
+              </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all mockups uikits logos">
+              <a class="popup-img" href="{{asset('images/cisde/logo2.webp')}}">
+                <img src="{{asset('images/cisde/logo2.webp')}}"  alt="Logo 2" height="200px">
+              </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all branding webdesig paginas">
+              <a class="popup-img" href="{{asset('images/cisde/cosmeticos.webp')}}">
+                <img src="{{asset('images/cisde/cosmeticos.webp')}}" alt="cosmeticos nicolas chamorro" height="200px">
+              </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all mockups webdesign tarjetas">
+              <a class="popup-img" href="{{asset('images/cisde/angela tarjeta.webp')}}">
+                <img src="{{asset('images/cisde/angela tarjeta.webp')}}" alt="angela" height="200px">
+              </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all branding uikits marketing">
+              <a class="popup-img" href="{{asset('images/cisde/instagram.webp')}}">
+                <img src="{{asset('images/cisde/instagram.webp')}}" alt="instagram" height="200px">
+              </a>
+            </div>
+
+            <div class="col-lg-4 col-md-6 portfolio-thumbnail all mockups uikits logos">
+              <a class="popup-img" href="{{asset('images/cisde/logo3.webp')}}">
+                <img src="{{asset('images/cisde/logo3.webp')}}" alt="logo 3" height="200px">
+              </a>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <!-- End section portfolio -->
+
+  <a href="https://wa.me/+573153488912" target="_blank" class="whatsapp">
+    <img src="{{asset('images/whatsapp.webp')}}" alt="whatsapp" height="60px">
+  </a>
+
+  <!-- start section footer -->
+  <div id="footer" class="text-center">
+    <div class="container">
+      <div class="socials-media text-center">
+
+        <ul class="list-unstyled">
+          <li><a href="https://www.facebook.com/Cisde.web"><i class="ion-social-facebook"></i></a></li>
+          <li><a href="https://www.instagram.com/cisdewdp"><i class="ion-social-instagram"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/cisdeweb/"><i class="ion-social-linkedin"></i></a></li>
+        </ul>
+
+      </div>
+
+      <p>
+        <a href="https://cisde.co" target="_blank">
+          &copy; Copyrights CISDE. All rights reserved.
+        </a>
+      </p>
+
+    </div>
+  </div>
+  <!-- End section footer -->
+
+  <!-- JavaScript Libraries -->
+  <script src="{{asset('js/cisde/lib/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('js/cisde/lib/jquery/jquery-migrate.min.js')}}"></script>
+  <script src="{{asset('js/cisde/lib/typed/typed.js')}}"></script>
+  <script src="{{asset('js/cisde/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('js/cisde/lib/magnific-popup/magnific-popup.min.js')}}"></script>
+  <script src="{{asset('js/cisde/lib/isotope/isotope.pkgd.min.js')}}"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="{{asset('js/cisde/main.js')}}" rel="preload"></script>
+
+</body>
+
+</html>
